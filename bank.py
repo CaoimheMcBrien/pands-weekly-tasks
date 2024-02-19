d38a9635dd8fd1e08c0d847b7f8167d919835063
CaoimheMcBrien <159313476+CaoimheMcBrien@users.noreply.github.com>
2024-02-19 19:28:13 +0000

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,24 @@
+#bank.py
+#weekly task adding two numbers in cents. Not quite sure but will give it a go. 
+#author: Caoimhe McBrien 
+
+print ("bank.py")
+
+print ("65+180")
+
+print (65+180)
+print(.65 + 1.80)
+
+#I feel like it can't be as easy as including a euro sign to complete this task but it feels like the best idea at the moment. 
+
+print (2.45)
+       
+#ok, that doesn't work. back to the drawing board. 
+
+number = 2.45
+
+curr= "{:}". format (number)
+
+print ('€', curr)
+
+#Did I get it? Not sure but I do see €2.45 at the end of my terminal. I did need to search on google.    
