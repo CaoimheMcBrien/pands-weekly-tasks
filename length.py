710447ce1b464a89c5061af1d5baafdc4e8049c9
Caoimhe McBrien <caoimhemcbrien@gmail.com>
2024-03-06 15:28:16 +0000

    Week 3 Lab

---
+++ length.py
@@ -0,0 +1,16 @@
+# Weekly Task 3
+#Using strings.
+# Finding the last 4 digits of an account number. 
+# Author Caoimhe McBrien 
+astring = "hello"
+print (astring)
+inputstring= input ('enter a string: ')
+lenghtofstring = len (inputstring)
+print (f'The lenght of {inputstring} is {lenghtofstring}')
+
+#%%
+# Simple program creating and outputting a string 
+#Author Caoimhe McBrien
+message = 'John said\t"hi"\nI said\t\t"bye"'
+print (message)
+#%%
